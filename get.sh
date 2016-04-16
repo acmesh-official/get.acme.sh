e75b99c6c2a88228fa5219bc00b721b06a992291
neil <git@byneil.com>
2016-04-16 19:39:57 +0800

    fix compatible for sh

--- get.sh
+++ get.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
 
+_exists() {
+  cmd="$1"
+  if [[ -z "$cmd" ]] ; then
+    _err "Usage: _exists cmd"
+    return 1
+  fi
+  if type command >/dev/null 2>&1 ; then
+    command -v $cmd >/dev/null 2>&1
+  else
+    type $cmd >/dev/null 2>&1
+  fi
+  ret="$?"
+  _debug2 "$cmd exists=$ret"
+  return $ret
+}
 
-if command -v curl >/dev/null 2>&1 ; then
+if _exists curl ; then
   curl https://raw.githubusercontent.com/Neilpang/acme.sh/master/acme.sh | INSTALLONLINE=1  bash
-elif command -v wget >/dev/null 2>&1 ; then
+elif _exists wget ; then
   wget -O -  https://raw.githubusercontent.com/Neilpang/acme.sh/master/acme.sh | INSTALLONLINE=1  bash
 else
   echo "Sorry, you must have curl or wget installed first."
